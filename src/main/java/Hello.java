9f1e559d3ffc9314bb9b16aa0d69388353653527
Berzhanin Dmitry <di.berzhanin@gmail.com>
2022-04-26 23:26:49 +0300

    create new maven project

---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * Description:
+ *
+ * @author DmitryBerzhanin
+ * @date 4/26/2022
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
